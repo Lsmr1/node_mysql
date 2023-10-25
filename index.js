a1710efdf4f944417cff705c2e4cf2624470afcf
Leonardo Salinas <39696777+Lsmr1@users.noreply.github.com>
2023-10-24 22:43:19 -0300

    primeira conexao como my sql

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const exprhbs = require("express-handlebars")
+const mysql = require("mysql2")
+
+const app = express()
+
+// definindo o handlebar como template engine
+app.engine ("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+// past de arquivos estáticos com CSS, imagens
+app.use(express.static("public"))
+
+// trabalhar com dados no  formato jason
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+// rotas
+app.get('/', (requisicao, resposta) => {
+    resposta.render("home")
+})
+
+// conexão com o mySQL
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql",
+    port: 3307
+})
+
+conn.connect((error) => {
+    if (error) {
+        console.log(error)
+        return
+    }
+
+    console.log("Conectando ao MySQL!")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000!")
+    })
+})
